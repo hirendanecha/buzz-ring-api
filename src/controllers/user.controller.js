8a70cd682c2975b50f5e98231a33f1c1c1a4f3ed
Swapnil Opash <sra.opash@gmail.com>
2024-05-23 18:31:04 +0530

    fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -169,7 +169,7 @@ exports.callNotification = async function (req, res) {
       const message = {
         data: { title: "call notification", body: JSON.stringify(messageData) },
         token: element?.fcmToken,
-        timeToLive: 30,
+        // timeToLive: 30,
         android: {
           priority: "high",
           // content_available: true, // Android-specific
@@ -222,7 +222,7 @@ exports.groupCallNotification = async function (req, res) {
             body: JSON.stringify(messageData),
           },
           token: element?.fcmToken,
-          timeToLive: 30,
+          // timeToLive: 30,
           android: {
             priority: "high",
             // content_available: true, // Android-specific
